3bbcbd6b13ab9b3e61aa66536c4188da8b9f9c55
Sorcerer913 <7964277@gmail.com>
2021-10-09 15:28:55 +0300

    Deleted unused part of CustomView

--- app/src/main/java/com/achievka/moretech3/extentions/Context.kt
+++
@@ -1,7 +0,0 @@
-package com.achievka.moretech3.extentions
-
-import android.content.Context
-
-fun Context.dpToPx(dp: Int) : Float {
-    return dp.toFloat() * this.resources.displayMetrics.density
-}
